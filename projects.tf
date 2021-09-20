96df35208050bdb7e618c2d5661b45b9a422b847
alex-kay <ichijoint@gmail.com>
2021-09-20 09:45:21 +0300

    added second stage graph

--- projects.tf
+++ projects.tf
@@ -19,28 +19,28 @@
 #   # ]
 # }
 
-variable "billing_account" {
-  description = "The ID of the billing account to associate this project with"
-  default = "01E9B9-208B76-F0CB90"
-}
+# variable "billing_account" {
+#   description = "The ID of the billing account to associate this project with"
+#   default = "01E9B9-208B76-F0CB90"
+# }
 
-variable "activate_apis" {
-  description = "Service APIs to enable."
-  type        = list(string)
-  default     = ["compute.googleapis.com"]
-}
+# variable "activate_apis" {
+#   description = "Service APIs to enable."
+#   type        = list(string)
+#   default     = ["compute.googleapis.com"]
+# }
 
-module "project-factory" {
-  source                  = "terraform-google-modules/project-factory/google"
-  version = "~> 10.1"
-  random_project_id       = true
-  name                    = "simple-sample-project"
-  org_id                  = "0"
-  billing_account         = var.billing_account
-  default_service_account = "deprivilege"
+# module "project-factory" {
+#   source                  = "terraform-google-modules/project-factory/google"
+#   version = "~> 10.1"
+#   random_project_id       = true
+#   name                    = "simple-sample-project"
+#   org_id                  = "0"
+#   billing_account         = var.billing_account
+#   default_service_account = "deprivilege"
 
-  usage_bucket_name    = "pf-test-1-usage-report-bucket"
-  usage_bucket_prefix  = "pf/test/1/integration"
+#   usage_bucket_name    = "pf-test-1-usage-report-bucket"
+#   usage_bucket_prefix  = "pf/test/1/integration"
 
-  activate_apis = var.activate_apis
-}
+#   activate_apis = var.activate_apis
+# }
